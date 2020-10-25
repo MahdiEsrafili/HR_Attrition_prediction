7251c99f9a01cf03de48b7fe0f51082c91d0c482
MahdiEsrafili <mahdi73.es12@gmail.com>
2020-10-26 00:03:26 +0330

    Revert "recommendation analyze added bot not tested"

--- Recommendation.py
+++ Recommendation.py
@@ -1,20 +1,13 @@
 from sklearn.neighbors import DistanceMetric, NearestNeighbors
 class Recommend:
-    search_on = ['Age', 'EducationField', 'JobRole', 'MaritalStatus', 'Department',
+    search_on = ['Age', 'EducationField', 'MaritalStatus', 'Department',
                 'NumCompaniesWorked', 'TrainingTimesLastYear',
                 'YearsAtCompany', 'YearsInCurrentRole',
                 'YearsSinceLastPromotion','YearsWithCurrManager']
-
-    analyze_on = ['BusinessTravel', 'DailyRate', 'DistanceFromHome',
-                  'EnvironmentSatisfaction', 'HourlyRate', 'JobInvolvement',
-                  'MonthlyIncome', 'MonthlyRate', 'OverTime', 'PercentSalaryHike',
-                  'PerformanceRating', 'RelationshipSatisfaction', 'StockOptionLevel',
-                  'WorkLifeBalance']
     def __init__(self, data, target):
         self.main_data = data.loc[data[target]==0]
         self.data = data.loc[data[target]==0][self.search_on]
         self.knn = NearestNeighbors().fit(self.data)
     def recommend(self, sample):
         distance, indx = self.knn.kneighbors(sample[self.search_on])
-        similar_persons = self.main_data.iloc[indx[0]]
-        return similar_persons
+        return self.main_data.iloc[indx[0]]
