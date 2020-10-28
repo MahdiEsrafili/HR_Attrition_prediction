361ecae1bd91c8a5da8ccfb151535db2aae35849
mahdi_es <mahdi73.es12@gmail.com>
2020-10-29 00:46:15 +0330

    wraps model creation, prediction, recommendation, recommendation analysis toghether. adds logging

--- ml_connector.py
+++ ml_connector.py
@@ -1,9 +1,14 @@
 from ml_core import ML_core
+from Recommendation import Recommend
+from recommend_analyzer import Analyze
 import pandas as pd
 from kafka import KafkaProducer
 from kafka import KafkaConsumer
 from io import StringIO
+import time
+import logging
 
+logging.basicConfig(filename='ml_connector.log', filemode='a', format='%(name)s - %(levelname)s - %(message)s', level = logging.INFO)
 
 consumer = KafkaConsumer(
     'ml_*',
@@ -24,10 +29,29 @@ for message in consumer:
     else:
         pass 
 
-# train_data = pd.read_csv('data/name_attached/train_data_incl_names.csv')
-# predict_data = pd.read_csv('data/name_attached/prediction_data_incl_names.csv') 
-# ml_core = ML_core(train_data, predict_data)
-# ml_core.train()
-# result = ml_core.predict()
-# result.to_csv('runs/prediction_result.csv')]
+def train_predict_recommend(train_data, predict_data):
+    start_time = time.time()
+    none_informative = ['Name', 'Unnamed: 0']
+    train_data = train_data.drop(none_informative, 1)
+    predict_data = predict_data.drop(none_informative, 1)
+    ml_core = ML_core(train_data, predict_data)
+    ml_core.train()
+    result = ml_core.predict()
+    train_data_led = ml_core.label_encoder.transform(train_data)
+    recommender = Recommend(train_data_led, 'Attrition')
+    need_recommendation = result[result.Attrition==1]
+    recommends_list = []
+    for person_indx in range(need_recommendation.shape[0]):
+        person = ml_core.label_encoder.transform(need_recommendation.iloc[person_indx:person_indx+1])
+        recoms = recommender.recommend(person)
+        analyzer = Analyze(need_recommendation.iloc[person_indx:person_indx+1], train_data.iloc[recoms.index])
+        an = analyzer.analyze()
+        recommends_list.append(an)
+        
+    result['recommendations'] = 'NaN'
+    result.loc[result.Attrition==1, 'recommendations'] = recommends_list
+    end_time = time.time()
+    logging.info(f'recommend at {end_time - start_time:.3f}')
+    return result
+    
 
