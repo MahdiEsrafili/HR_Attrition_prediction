e0314e8ba6f89d1b6abe78481d2f206226a7e4fc
mahdi_es <mahdi73.es12@gmail.com>
2020-11-11 16:10:34 +0330

    changes db setting to heroku psql settings

--- chl_app_config.py
+++ chl_app_config.py
@@ -1,5 +1,10 @@
 
-db_string = "postgres://mluser:123456789@localhost:5432/mldb"
+user = 'ilhlwtaaiqfkgy'
+pswd = '651864ace94b45772563fe9113fd1eb82062bb401e87b1de7f0fadd585d565d6'
+port = 5432
+database = 'ddsp5d33leitdc'
+host = 'ec2-52-17-53-249.eu-west-1.compute.amazonaws.com'
+db_string = f"postgres://{user}:{pswd}@{host}:{port}/{database}"
 train_table_name = 'training_data'
 predict_table_name = 'prediction_data'
 upload_dir = 'upload_dir'
